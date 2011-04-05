7c7ecb5fd3d0ee44046604f2c2883f22c8d62800
Nick Gerner <nick.gerner@gmail.com>
2011-04-04 18:48:42 -0700

    changing layout, with partial application of arthemia theme to site

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,8 @@ Events::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "home#index"
+  #root :to => "home#index"
+  root :to => "events#index"
   match "search" => "events"
 
   # See how all your routes lay out with "rake routes"
