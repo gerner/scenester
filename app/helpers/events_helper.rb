a6cf3ab258614b9cf61506ef3f494f7bb6d46316
Nick Gerner <nick.gerner@gmail.com>
2011-05-15 12:37:07 -0700

    separating events by the day they occur on

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -10,4 +10,20 @@ module EventsHelper
   def add_to_google_calendar(e)
     "<a href=\"http://www.google.com/calendar/event?action=TEMPLATE&text=#{CGI::escape(e.title)}&dates=#{e.start.utc.strftime("%Y%m%dT%H%M%SZ")}/#{e.end.utc.strftime("%Y%m%dT%H%M%SZ")}&location=#{CGI::escape(venue_name_address(e))}\"><img class=\"gcalbtn\" src=\"http://www.google.com/calendar/images/ext/gc_button6.gif\"></a>".html_safe
   end
+
+  def group_events_by_day(events)
+    events.group_by do |e|
+      e.start.beginning_of_day
+    end
+  end
+
+  def day_separator(date)
+    if date.beginning_of_day == DateTime.now.beginning_of_day
+      "Today"
+    elsif date.beginning_of_day == DateTime.now.beginning_of_day.advance(:days => 1)
+      "Tomorrow"
+    else
+      date.strftime("%b %d")
+    end
+  end
 end
