e5510265fae0a757fbf2d945b72f5f79026e4687
Nick Gerner <nick.gerner@gmail.com>
2011-05-24 09:13:08 -0700

    if in production, use environment variables for oauth creds

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,13 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'RsWS1vSD0Dhfyo7qfCZF6w', 'PR8OKXz8qumiY6IZf4btxkcswpzph6lrOAwdPLxcs'
-  provider :facebook, '61b9cfe8ca576b4b0bbc90b172d4da25', '10b0c17bfae1f4b8ac814a84299a282c'
-  provider :foursquare, 'RPB4EENJOL3FSOZVKYZSOHS1034IIDYQJM5VCGIIZEBWWOA1', 'LGYDXOYGPTUNJZJ2G3SJ10VFTXLRACTM4FNPQB4QW0PQNWGS'
+if Rails.env == "production"
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+    provider :foursquare, ENV['FOURSQUARE_KEY'], ENV['FOURSQUARE_SECRET']
+  end
+else
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :twitter, 'RsWS1vSD0Dhfyo7qfCZF6w', 'PR8OKXz8qumiY6IZf4btxkcswpzph6lrOAwdPLxcs'
+    provider :facebook, '61b9cfe8ca576b4b0bbc90b172d4da25', '10b0c17bfae1f4b8ac814a84299a282c'
+    provider :foursquare, 'RPB4EENJOL3FSOZVKYZSOHS1034IIDYQJM5VCGIIZEBWWOA1', 'LGYDXOYGPTUNJZJ2G3SJ10VFTXLRACTM4FNPQB4QW0PQNWGS'
+  end
 end
