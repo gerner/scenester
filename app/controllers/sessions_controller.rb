40bf8dd89da95e6f1cee76f71c610b1250c0484f
Nick Gerner <nick.gerner@gmail.com>
2011-07-12 08:53:23 -0700

    disabling creation of new users for the moment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,11 @@ class SessionsController < ApplicationController
     unless @auth = Authorization.find_from_hash(auth)
       # Create a new user or add an auth to existing user, depending on
       # whether there is already a user signed in.
-      @auth = Authorization.create_from_hash(auth, current_user)
+      # TODO: for now we don't want to allow creation of new users
+      # @auth = Authorization.create_from_hash(auth, current_user)
+      flash[:notice] = "We're not currently allowing new users to sign up. But you can still use our site to find things to do without having an account!"
+      redirect_back_or_default("/")
+      return
     end
     # Log the authorizing user in.
     self.current_user = @auth.user
